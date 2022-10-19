5d92f99488d869ea8f335df59b05e013f193b9ac
Alexis Graff <alexisgraff123@gmail.com>
2022-10-19 08:32:27 -0300

    borrando console.log

--- src/components/muiForecast/LoadingData.jsx
+++ src/components/muiForecast/LoadingData.jsx
@@ -2,7 +2,6 @@ import {Box} from "@mui/material";
 import {JellyTriangle} from "@uiball/loaders";
 
 export default function LoadingData() {
-    console.log('entro loading data')
     return(
         <Box mt={4} width={1} display='flex' justifyContent='center'>
             <JellyTriangle size={35} color="#231F20" />
